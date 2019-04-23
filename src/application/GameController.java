6e450a895d2404b0ea5811f2d17d6f449b994c29
synergievdp <synergievdp@gmail.com>
2019-04-23 15:59:08 +0200

    View bind properties, cpm, start animation

--- src/application/GameController.java
+++ src/application/GameController.java
@@ -3,7 +3,7 @@ package application;
 import java.util.*;
 
 import application.model.*;
-
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -14,20 +14,42 @@ public class GameController extends Application {
 	View view = new View();
 	String output = new Level(LevelData.LEVEL1).getOutput();
 	HashMap<String, Image> keyboard = keyboard();
-	int curChar = 0, mistakes = 0;
+	int curChar = 0;//, mistakes = 0;
+	long timeStamp, cpm = 0;
+	boolean anim = false;
+	AnimationTimer at;
 
 	@Override
     public void start(Stage primaryStage) {
+		// FIXME Level select, 80% accuracy
+		//Level level = new Level(LevelData.LEVEL1);
+		//level.setNumberOfCharstoType(int);
+		//String output = level.getOutput();
     	view.setAmount(output.length());
     	Scene scene = view.init();
 
-    	for(int i = 0; i < view.text.getChildren().size(); i++) {
-    		view.setText(((Label)view.text.getChildren().get(i)), output.charAt(i)+"");
+    	for(int i = 0; i < output.length(); i++) {
+    		view.setText(view.getLabel(i), output.charAt(i)+"");
     	}
-    	view.text.getChildren().get(curChar).setStyle("-fx-border-width: 1; -fx-border-color: #000000;");
+    	view.getLabel(curChar).setStyle("-fx-border-width: 1; -fx-border-color: #000000;");
     	
-    	view.setText(view.mistakes, "mistakes = " + mistakes);
-    	view.setImage(keyboard.get(output.charAt(curChar)+""));
+    	if(!anim) view.setImage(keyboard.get(output.charAt(curChar)+""));
+    	else {
+    		AnimationTimer at = new AnimationTimer() {
+    			int frameCount = 0;
+    		    
+	            @Override
+	            public void handle(long currentNanoTime) {
+	               if (frameCount % 6 == 0) {
+	            	   Label label = view.getLabel(curChar);
+	            	   // FIXME 300 is an arbitrary number (scene height / 2)
+	            	   label.setTranslateY(label.getTranslateY()>=300?0:label.getTranslateY()+10);
+	               }         
+	               frameCount++;
+	            }
+    		};
+    		at.start();
+    	}
     	
     	primaryStage.setScene(scene);
     	scene.setOnKeyPressed(ke -> onKeyPressed(ke.getText()));
@@ -35,20 +57,33 @@ public class GameController extends Application {
     }
 
     void onKeyPressed(String ke) {
+    	// process keyboard input
     	char[] array = output.toCharArray();
     	if(ke.equals(array[curChar]+"")) {
-    		view.text.getChildren().get(curChar).setStyle("-fx-background-color: #00FF00;");
-    		curChar=(curChar<array.length-1)?curChar+1:0;
-    		if (curChar == 0) view.text.getChildren().stream().forEach(n -> n.setStyle(null)); 
-    		view.text.getChildren().get(curChar).setStyle("-fx-border-width: 1; -fx-border-color: #000000;");
-    		view.setImage(keyboard.get(output.charAt(curChar)+""));
+    		view.getLabel(curChar).setStyle("-fx-background-color: #00FF00;");
+    		if(anim) view.getLabel(curChar).setTranslateY(0);
+    		
+    		// To continue : move the following lines outside if-statement
+    		curChar=(curChar<array.length-1)?curChar+1:0; 
+    		//FIXME if (curChar == 0) view.text.getChildren().stream().forEach(n -> n.setStyle(null)); 
+    		view.getLabel(curChar).setStyle("-fx-border-width: 1; -fx-border-color: #000000;");
+    		if(!anim) view.setImage(keyboard.get(output.charAt(curChar)+""));
     	}
+    	
     	else {
-    		view.text.getChildren().get(curChar).setStyle("-fx-background-color: #FF0000");
-    		mistakes++; 
-    		view.setText(view.mistakes, "mistakes = " + mistakes);
+    		view.getLabel(curChar).setStyle("-fx-background-color: #FF0000");
+    		view.setMistakes(view.getMistakes()+1);
+    		// TODO Remember which chars go wrong the most
     	}
-    	view.setText(view.lastTyped, "last typed = " + ke);
+    	view.setLastTyped("last typed = " + ke);
+    	
+    	// characters per minute
+    	if (curChar == 0) {
+    		view.setCpm((output.length()/((1.0*System.nanoTime()-timeStamp))*60_000_000_000d));
+    		cpm = 0;
+    	}
+    	if(cpm == 0) timeStamp = System.nanoTime();
+    	cpm++; //counts wrong chars as well
     }
 
     HashMap keyboard() {
--- src/application/View.java
+++ src/application/View.java
@@ -1,5 +1,6 @@
 package application;
 
+import javafx.beans.property.*;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
@@ -9,25 +10,52 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 
 public class View {
-	HBox text = new HBox();
-	Label mistakes = new Label("mistakes = 0");
-	Label lastTyped = new Label("last typed = ");
-	ImageView keyboard = new ImageView();
-	int charAmount;
+	
+	private HBox text = new HBox();
+	private IntegerProperty mistakes = new SimpleIntegerProperty();
+	private DoubleProperty cpm = new SimpleDoubleProperty();
+	private StringProperty lastTyped = new SimpleStringProperty("last typed = ");
+	private ImageView keyboard = new ImageView();
+	private int charAmount;
+	
+//	private String 	border = "-fx-border-width: 1; -fx-border-color: #000000;",
+//					red = "-fx-background-color: #FF0000",
+//					green = "-fx-background-color: #00FF00;";
 	
 	Scene init() {
-		VBox root = new VBox(10);
+		VBox root = new VBox(50);
 		root.setStyle("-fx-font-size: 20px;");
+		
+		VBox topLeft = new VBox();
+		
+		Label mistakesLabel = new Label("mistakes = 0");
+		mistakesLabel.textProperty().bind(mistakes.asString());
+		
+		Label cpmLabel = new Label("cpm = 0");
+		cpmLabel.textProperty().bind(cpm.asString());
+		
+		Label lastTypedLabel = new Label("last typed = ");
+		lastTypedLabel.textProperty().bind(lastTyped);
+		
+		topLeft.getChildren().addAll(mistakesLabel, cpmLabel, lastTypedLabel);
 		
 		for(int i = 0; i < charAmount; i++) {
 			text.getChildren().add(new Label(""));
 		}
 		text.setAlignment(Pos.CENTER);
 		
-		root.getChildren().addAll(mistakes, text, lastTyped, keyboard);
-		root.setAlignment(Pos.CENTER);
+		VBox middle = new VBox(50);
+		middle.getChildren().addAll(text, keyboard);
+		middle.setAlignment(Pos.CENTER);
+		middle.setStyle("-fx-font-size: 30px;");
 		
-		return new Scene(root);
+		root.getChildren().addAll(topLeft, middle);
+		
+		return new Scene(root, 800, 600);
+	}
+	
+	Label getLabel(int index) {
+		return (Label) text.getChildren().get(index);
 	}
 	
 	void setText(Label label, String s) {
@@ -40,5 +68,47 @@ public class View {
 	
 	void setAmount(int amount) {
 		charAmount = amount;
+	}
+
+	public final IntegerProperty mistakesProperty() {
+		return this.mistakes;
+	}
+	
+
+	public final int getMistakes() {
+		return this.mistakesProperty().get();
+	}
+	
+
+	public final void setMistakes(final int mistakes) {
+		this.mistakesProperty().set(mistakes);
+	}
+
+	public final StringProperty lastTypedProperty() {
+		return this.lastTyped;
+	}
+	
+
+	public final String getLastTyped() {
+		return this.lastTypedProperty().get();
+	}
+	
+
+	public final void setLastTyped(final String lastTyped) {
+		this.lastTypedProperty().set(lastTyped);
+	}
+
+	public final DoubleProperty cpmProperty() {
+		return this.cpm;
+	}
+	
+
+	public final double getCpm() {
+		return this.cpmProperty().get();
+	}
+	
+
+	public final void setCpm(final double cpm) {
+		this.cpmProperty().set(cpm);
 	}
 }
